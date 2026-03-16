6ab2b5998b8e44a88530095bc050050c1094a7a9
yossi shapira <yosi105009@gmail.com>
2026-03-16 15:13:16 +0200

    import cors

--- api/app.js
+++ api/app.js
@@ -2,11 +2,12 @@ import 'dotenv/config'
 import express from 'express'
 import launcherRouter from './routes/launcherRoutes.js'
 import {connect} from "./db.js"
+import cors from "cors"
 const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json());
-
+app.use(cors())
 app.use('/api',launcherRouter)
 
 
