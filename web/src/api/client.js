fa1b52c7e7ad0ccab0fb4833b11b70259e9e7776
yossi shapira <yosi105009@gmail.com>
2026-03-16 16:26:36 +0200

    fetch

---
+++ web/src/api/client.js
@@ -0,0 +1,46 @@
+const BASE_URL = 'http://localhost:3010'
+
+export const getAllLaunchers = async (path) => {
+    try {
+        const response = await fetch(`${BASE_URL}/${path}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch launcher');
+        }
+
+        const data = await response.json();
+        return data
+    } catch (error) {
+        return error.message;
+    }
+}
+
+export const getLaunchersById = async (path, id) => {
+    const response = await fetch(`${BASE_URL}/${path}/${id}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch launcher');
+    }
+
+    const data = await response.json();
+    return data
+
+}
+
+export const deleteLaunchers = async (path, id) => {
+    const response = await fetch(`${BASE_URL}/${path}/${id}`, {
+        method: 'DELETE',
+    });
+    if (!response.ok) {
+        throw new Error('Failed to fetch launcher');
+    }
+    return await response.json();
+
+}
+
+export const createLaunchers = async (path, launcher) => {
+    const response = await fetch(`${BASE_URL}/${path}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(launcher)
+    });
+    return await response.json()
+}
